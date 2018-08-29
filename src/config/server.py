deb969b5427168c004b0acbe129c852e05cc8306
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2018-08-29 11:40:47 -0500

    Update server.py

--- src/config/server.py
+++ src/config/server.py
@@ -6,66 +6,57 @@ import time
 import os
 import json
 import psycopg2
+import src.libs.logger as logger
+import src.libs.commons as commons
 from flask import Flask, request, g, Response
+from flask_jsonschema import JsonSchema
+from flask_cors import CORS
 from werkzeug.serving import WSGIRequestHandler
 from src.config.logs import config_logs
 from src.config.database import get_database
 from src.monkeypatch.request_logs import werk_log
-from flask_jsonschema import JsonSchema, ValidationError
-from flask_cors import CORS
 from src.libs.io import IO
-import src.libs.logger as logger
-import src.libs.commons as commons
+
 
 """
 App register section
 """
 APP = Flask(__name__)
-CORS(APP)
-
 config_logs(APP)
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+"""
+Cors middeware
+"""
+CORS(APP)
 
+"""
+JsonSchema configuration
+"""
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 APP.config['JSONSCHEMA_DIR'] = DIR_PATH + '/../schemas'
 
 JSON_SCHEMA = JsonSchema(APP)
 
-DB = get_database()
 
 """
 Patch section
 """
 WSGIRequestHandler.log = werk_log
 
+"""
+Functions that returns configuration objects
+"""
+
+def get_app():
+    return APP
+
+def get_jsonschema():
+    return JSON_SCHEMA
+
 
 """
 Hooks for the flask core
 """
-
-@APP.errorhandler(psycopg2.IntegrityError)
-def integrity_error(e):
-    logger.log({'error': str(e)})
-
-    return IO.response({
-        'code': 409,
-        'message': 'integrity error',
-        'data': {
-            'trace': commons.parse_error(e)
-        }
-    })
-
-@APP.errorhandler(ValidationError)
-def on_validation_error(e):
-    logger.log({'error': str(e)}, 'ERROR')
-
-    return IO.response({
-        'code': 422,
-        'message': 'unprosesable data',
-        'data': {
-            'trace': commons.parse_error(e)
-        }
-    })
 
 @APP.before_request
 def before_request():
@@ -125,20 +116,7 @@ def after_request(response):
         json.dumps(data)
     )
 
-    APP.logger.debug(request_log)
+    logger.get_handler().debug(request_log)
 
     return response
 
-
-"""
-Functions that returns configuration objects
-"""
-
-def get_app():
-    return APP
-
-def get_jsonschema():
-    return JSON_SCHEMA
-
-def get_database():
-    return DB
